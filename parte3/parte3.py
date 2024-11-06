1b2b48274b2791800a16af06b1b8d078d38192cb
laucha <ldenobili@fi.uba.ar>
2024-11-06 14:56:40 -0300

    Arregle el vocabulario usado y cambie la demostracion de NP-Completitud a satisfacier un k de vertices minimos en el Independent Set.

--- parte3/parte3.py
+++ parte3/parte3.py
@@ -5,31 +5,35 @@ import sys
 1) Demostrar que el Problema de la Batalla Naval se encuentra en NP: 
         Un problema NP es aquel problema para el que existe un certificador eficiente, es decir, que se puede validar 
     en  tiempo polinomial. 
-        Para demostrar que el Problema de la Batalla Naval se encuentra en NP, debemos proponer un certificador que 
-    valide su resultado en tiempo polinomial (no tiene que resolverlo, solo validar lo que devolvio). En nuestro caso, 
-    devuelve un tablero, que para validarlo podemos recorrer todas sus casillas en O(n x m), disminuyendo en 1 los 
+        Para demostrar que el Problema de la Batalla Naval se encuentra en NP, se debe proponer un certificador que 
+    valide su resultado en tiempo polinomial (no tiene que resolverlo, solo validar lo que devolvio). En este caso, 
+    devuelve un tablero, que para validarlo se pueden recorrer todas sus casillas en O(n x m), disminuyendo en 1 los 
     valores de demanda, en sus respectivos indices, en las listas de fila y columna, si la casilla que se esta
     recorriendo esta ocupada. Terminado el recorrido de todo el tablero, se hace la sumatoria de ambas listas, que 
     quedarian siendo las demandas de fila y columna  insatisfechas, en O(n) y O(m) respectivamente, dando como 
     resultado a la validacion la cantidad de demandas insatisfechas. Esta comprobacion tambien puede devolver, digamos,
     -1 como flag para informar que hubo al menos una fila o columna en la cual se ocuparon demasiados casilleros (mas
     que lo pedido por la demanda).
+        La previa demostracion no es exclusiva, implicando que pueden haber otros validadores (y asi demostraciones)
+    para el Problema de la Batalla Naval. Otro ejemplo de un validador puede ser validar la cantidad de barcos colocados
+    en las filas, tal se usaria en la demostracion siguiente.
        
 2) Demostrar que el Problema de la Batalla Naval es un problema NP-Completo:
         Un problema NP-Completo es aquel que se encuentra en NP y, al mismo tiempo, todos los problemas NP pueden 
     reducirse a el. En criollo, NP-Completo son problemas very difficult.
         Si un problema NP-Completo puede reducirse a un problema X, entonces, dicho problema X es NP-Completo, por
     propiedad de transitividad.
-        Para demostrar que el Problema de la Batalla Naval es un problema NP-Completo, necesitamos reducir un problema 
-    Y NP-Completo, que podamos demostrarlo (sea porque se vio en clase como tal o porque sin trampa lo podemos 
-    demostrar), a nuestro problema X de la Batalla Naval.
+        Para demostrar que el Problema de la Batalla Naval es un problema NP-Completo, se necesita reducir un problema 
+    Y NP-Completo, que se pueda demostrar (sea porque se vio en clase como tal o porque sin trampa se puede demostrar),
+    al problema X de la Batalla Naval.
         ~sujeto a modificacion para otra propuesta~
-        Proponemos reducir el problema de Independent Set (demostrada NP-Completitud en clase) al problema de la
+        Se propone reducir el problema de Independent Set (demostrada NP-Completitud en clase) al problema de la
     Batalla Naval.
-        Por parte del Independent Set, tenemos un grafo como parametro, con vertices, aristas y metodos (concretamente 
-    grafo.obtener_vertices() y grafo.adyacentes(vertice)), siendo N la cantidad de vertices de dicho grafo. Por parte de
-    Batalla Naval, tenemos un tablero, un array de barcos, un array de demandas de filas y un array de demandas de 
-    columnas. Para reducirlo proponemos:
+        Por parte del Independent Set, se tiene un grafo como parametro, con vertices, aristas y metodos (concretamente 
+    grafo.obtener_vertices() y grafo.adyacentes(vertice)), siendo N la cantidad de vertices de dicho grafo. Tambien se
+    posee de un valor K, donde el problema implica devolver True si la existe un Independent Set de al menos k verices.
+    Por parte de Batalla Naval, se tiene un tablero, un array de barcos, un array de demandas de filas y un array de 
+    demandas de columnas. Para reducirlo se propone:
         - un tablero de dimensiones NxN;
         - un arreglo de N barcos, donde cada i (es decir, cada longitud de barco) sea la cantidad de adyacentes al 
         vertice i.
@@ -38,9 +42,12 @@ import sys
         - un arreglo de demandas de columnas, donde cada j (es decir, cada demanda de columa) sea un numero muy grande,
         de manera tal que no se lograse satisfacer. Dicho numero muy grande puede ser tanto infinito como V+1. 
         La longitud de este arreglo es, nuevamente, N.
-    Esto nos devolveria el tablero, luego necesitariamos de un adaptador para saber cual es la mayor cantidad de 
-    vertices en el Independent Set para el cual necesitamos reducir a la Batalla Naval por ser un problema "muy grande".
-    Naturalmente, se podria lograr que el adaptador devolviese dicho set.
+    Esto resulta en el tablero con la mayor cantidad de barcos colocados, luego se precisa de un validador (eficiente y 
+    que valide en tiempo polinomial) para saber cuantos barcos colocados hay. Cada barco es un vertice contenido en el 
+    Independent set. Como ultimo, se compara esa cantidad de barcos colocados al valor K, devolviendose True si hay al
+    menos K barcos (vertices) en el tablero (el Independent Set).
+        Fuese otro el requisito del problema de Independent Set, como, por ejemplo, obtener dicho set, se podria armar
+    un set con los barcos ubicados en el tablero, que son, propiamente, los vertices en el set.
         El Problema de la Batalla Naval es un problema NP-Completo.
                                                                                                 quod erat demonstrandum
 """
